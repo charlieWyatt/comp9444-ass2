65c47d0729dd38204cfa88c805fa5e73bc91183d
Charlie Wyatt <charlie.wyatt0@icloud.com>
2021-11-13 16:49:00 +1100

    Not working. 1/8 chance of success

--- hw2/student.py
+++ hw2/student.py
@@ -36,31 +36,240 @@ def transform(mode):
     https://pytorch.org/vision/stable/transforms.html
     You may specify different transforms for training and testing
     """
+
+    # Transforms should do things like cropping, padding, normalizing etc.
     if mode == 'train':
         return transforms.ToTensor()
     elif mode == 'test':
         return transforms.ToTensor()
 
 
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# COPYING FROM RESNET HERE DOWN TO !!!!!!!!!!!!!!!
+
+class Conv2dAuto(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.padding =  (self.kernel_size[0] // 2, self.kernel_size[1] // 2) # dynamic add padding based on the kernel_size
+
+from functools import partial
+
+conv3x3 = partial(Conv2dAuto, kernel_size=3, bias=False)
+
+def activation_func(activation):
+    return  nn.ModuleDict([
+        ['relu', nn.ReLU(inplace=True)],
+        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+        ['selu', nn.SELU(inplace=True)],
+        ['none', nn.Identity()]
+    ])[activation]
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, activation='relu'):
+        super().__init__()
+        self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
+        self.blocks = nn.Identity()
+        self.activate = activation_func(activation)
+        self.shortcut = nn.Identity()   
+    
+    def forward(self, x):
+        residual = x
+        if self.should_apply_shortcut: residual = self.shortcut(x)
+        x = self.blocks(x)
+        x += residual
+        x = self.activate(x)
+        return x
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.out_channels
+
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=conv3x3, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1,
+                      stride=self.downsampling, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)) if self.should_apply_shortcut else None
+        
+        
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
+
+def conv_bn(in_channels, out_channels, conv, *args, **kwargs):
+    return nn.Sequential(conv(in_channels, out_channels, *args, **kwargs), nn.BatchNorm2d(out_channels))
+
+class ResNetBasicBlock(ResNetResidualBlock):
+    """
+    Basic ResNet block composed by two layers of 3x3conv/batchnorm/activation
+    """
+    expansion = 1
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, conv=self.conv, bias=False, stride=self.downsampling),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.expanded_channels, conv=self.conv, bias=False),
+        )
+class ResNetBottleNeckBlock(ResNetResidualBlock):
+    expansion = 4
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, expansion=4, *args, **kwargs)
+        self.blocks = nn.Sequential(
+           conv_bn(self.in_channels, self.out_channels, self.conv, kernel_size=1),
+             activation_func(self.activation),
+             conv_bn(self.out_channels, self.out_channels, self.conv, kernel_size=3, stride=self.downsampling),
+             activation_func(self.activation),
+             conv_bn(self.out_channels, self.expanded_channels, self.conv, kernel_size=1),
+        )
+
+class ResNetLayer(nn.Module):
+    """
+    A ResNet layer composed by `n` blocks stacked one after the other
+    """
+    def __init__(self, in_channels, out_channels, block=ResNetBasicBlock, n=1, *args, **kwargs):
+        super().__init__()
+        # 'We perform downsampling directly by convolutional layers that have a stride of 2.'
+        downsampling = 2 if in_channels != out_channels else 1
+        self.blocks = nn.Sequential(
+            block(in_channels , out_channels, *args, **kwargs, downsampling=downsampling),
+            *[block(out_channels * block.expansion, 
+                    out_channels, downsampling=1, *args, **kwargs) for _ in range(n - 1)]
+        )
+
+    def forward(self, x):
+        x = self.blocks(x)
+        return x
+
+class ResNetEncoder(nn.Module):
+    """
+    ResNet encoder composed by layers with increasing features.
+    """
+    def __init__(self, in_channels=3, blocks_sizes=[64, 128, 256, 512], deepths=[2,2,2,2], 
+                 activation='relu', block=ResNetBasicBlock, *args, **kwargs):
+        super().__init__()
+        self.blocks_sizes = blocks_sizes
+        
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_channels, self.blocks_sizes[0], kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.blocks_sizes[0]),
+            activation_func(activation),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        
+        self.in_out_block_sizes = list(zip(blocks_sizes, blocks_sizes[1:]))
+        self.blocks = nn.ModuleList([ 
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], activation=activation, 
+                        block=block,*args, **kwargs),
+            *[ResNetLayer(in_channels * block.expansion, 
+                          out_channels, n=n, activation=activation, 
+                          block=block, *args, **kwargs) 
+              for (in_channels, out_channels), n in zip(self.in_out_block_sizes, deepths[1:])]       
+        ])
+        
+        
+    def forward(self, x):
+        x = self.gate(x)
+        for block in self.blocks:
+            x = block(x)
+        return x
+
+class ResnetDecoder(nn.Module):
+    """
+    This class represents the tail of ResNet. It performs a global pooling and maps the output to the
+    correct class by using a fully connected layer.
+    """
+    def __init__(self, in_features, n_classes):
+        super().__init__()
+        self.avg = nn.AdaptiveAvgPool2d((1, 1))
+        self.decoder = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.decoder(x)
+        return x
+
+class ResNet(nn.Module):
+    
+    def __init__(self, in_channels, n_classes, *args, **kwargs):
+        super().__init__()
+        self.encoder = ResNetEncoder(in_channels, *args, **kwargs)
+        self.decoder = ResnetDecoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, n_classes)
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+def resnet50(in_channels, n_classes, block=ResNetBottleNeckBlock, *args, **kwargs):
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 ############################################################################
 ######   Define the Module to process the images and produce labels   ######
 ############################################################################
 class Network(nn.Module):
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         super().__init__()
+        self.encoder = ResNetEncoder(3, block = ResNetBottleNeckBlock, deepths = [3, 4, 6, 3], *args, **kwargs)
+        self.decoder = ResnetDecoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, 8)
         
-    def forward(self, input):
-        pass
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+    # def __init__(self):
+    #     # from assignment 1
+    #     super(Network, self).__init__()
+    #     self.conv1=nn.Conv2d(in_channels = 3, out_channels = 64,kernel_size = 5, padding=2) 
+    #     self.max_pool=nn.MaxPool2d(2,2) 
+    #     self.conv2=nn.Conv2d(in_channels =64 + 3,out_channels = 24,kernel_size = 5, padding=2) 
+    #     self.fc_layer_1=nn.Linear(9600,230)
+    #     self.fc_layer_2=nn.Linear(230,8)
+        
+    # def forward(self, input):
+    #     self.hid1 = F.relu(self.conv1(input))
+    #     layer2Input = torch.cat([input, self.hid1], dim = 1)
+    #     layer2 = self.max_pool(layer2Input)
+    #     self.hid2 = F.relu(self.conv2(layer2))
+    #     layer3Input = torch.cat([input, self.hid1, self.hid2], dim = 1)
+    #     layer4 = self.max_pool(layer3Input)
+    #     layer5 = layer4.view(layer4.size(0), -1)  #flattening the inputs. 
+    #     self.hid3 = F.relu(self.fc_layer_1(layer5))
+    #     layer4Input = torch.cat([input, self.hid1, self.hid2, self.hid3], dim = 1)
+    #     layer7 = self.fc_layer_2(layer4Input)
+    #     self.hid4 = F.log_softmax(layer7, dim=1)
+    #     return self.hid4
+  
+
+    # def forward(self, input):
+    #     input = input[-2]
+    #     print(input.shape)
+    #     self.hid1 = torch.tanh(self.lay1(input))
+    #     layer2Input = torch.cat([input, self.hid1], dim = 1)
+    #     self.hid2 = torch.tanh(self.lay2(layer2Input))
+    #     outInput = torch.cat([self.hid2, self.hid1, input], dim = 1)
+    #     return torch.sigmoid(self.lay3(outInput))
 
 net = Network()
     
 ############################################################################
 ######      Specify the optimizer and loss function                   ######
 ############################################################################
-optimizer = None
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5)
 
-loss_func = None
+def loss_def(input, target):
+    return F.nll_loss(input, target, reduction='sum')
+
+loss_func = loss_def
 
 
 ############################################################################
@@ -78,7 +287,7 @@ scheduler = None
 ############################################################################
 #######              Metaparameters and training options              ######
 ############################################################################
-dataset = "./data"
+dataset = "./data/data"
 train_val_split = 0.8
 batch_size = 200
 epochs = 10
