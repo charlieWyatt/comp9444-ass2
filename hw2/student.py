dc1f4df01e45ab32cf9822a5b8358b404b8c71d6
Charlie Wyatt <charlie.wyatt0@icloud.com>
2021-11-18 19:58:07 +1100

    50% model

--- hw2/student.py
+++ hw2/student.py
@@ -20,6 +20,8 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+import tensorflow.keras as keras
+
 """
    Answer to Question:
 
@@ -37,9 +39,27 @@ def transform(mode):
     You may specify different transforms for training and testing
     """
 
-    # Transforms should do things like cropping, padding, normalizing etc.
+    # Transforms should do things like cropping, padding, normalizing etc.\
+
+    # give ourselves more data
+    
     if mode == 'train':
-        return transforms.ToTensor()
+        # This happens at batch level, so it should increase the amount of data over multiple epochs
+        # maybe should use randomApply    
+        return transforms.Compose([
+            transforms.RandomChoice([ # chooses ONE of the below transformations (1-1/(transforms+1))% of the time. Should give an equal amount of transforms and normal data 
+                transforms.RandomHorizontalFlip(p = 1), # sometimes flips horizontally
+                transforms.RandomVerticalFlip(p = 1), # sometimes flips vertically
+                transforms.ColorJitter(brightness = 0.5), # changing the brightness a little bit
+                transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 5)), # A bit of blur
+                # transforms.Resize(80),
+                transforms.RandomRotation(degrees=(0, 360)), # randoms rotates some images
+                transforms.RandomAdjustSharpness(sharpness_factor=2), # randomly adjusts sharpness of image,
+                transforms.RandomAutocontrast(), 
+                # transforms.RandomResizedCrop(size = (60,60)), # not sure if this is the right size !!!!!!
+                transforms.ToTensor()
+            ], p = 0.88)
+        ])
     elif mode == 'test':
         return transforms.ToTensor()
 
@@ -51,42 +71,71 @@ class Network(nn.Module):
     def __init__(self):
         # from assignment 1
         super(Network, self).__init__()
-        self.conv1=nn.Conv2d(in_channels = 3, out_channels = 64,kernel_size = 5, padding=2) 
-        self.max_pool=nn.MaxPool2d(2,2) 
-        self.conv2=nn.Conv2d(in_channels =64,out_channels = 24,kernel_size = 5, padding=2) 
-        self.fc_layer_1=nn.Linear(9600,230)
-        self.fc_layer_2=nn.Linear(230,8)
+        self.conv1=nn.Conv2d(in_channels = 3, out_channels = 96,kernel_size = 11, stride = 4) 
+        self.batchNorm1 = nn.BatchNorm2d(96)
+        self.max_pool=nn.MaxPool2d(kernel_size = (3,3), stride = (2,2)) 
+        self.conv2=nn.Conv2d(in_channels =96, out_channels = 256, kernel_size = 5, stride = (1,1), padding = 5) 
+        self.batchNorm2 = nn.BatchNorm2d(256)
+        self.conv3=nn.Conv2d(in_channels =256, out_channels = 384, kernel_size = (3,3), stride = (1,1), padding = 5) 
+        self.batchNorm3 = nn.BatchNorm2d(384)
+        self.drop = nn.Dropout(0.5)
+        self.fc_layer_1=nn.Linear(75264,4096)
+        self.fc_layer_2=nn.Linear(4096,8)
+
         
     def forward(self, input):
         input = F.relu(self.conv1(input))
+        batchNorm = self.batchNorm1
+        input = batchNorm(input)
         input = self.max_pool(input)
         input = F.relu(self.conv2(input))
+        batchNorm = self.batchNorm2
+        input = batchNorm(input)
         input = self.max_pool(input)
+        input = self.conv3(input)
+        batchNorm = self.batchNorm3
+        input = batchNorm(input)
         input = input.view(input.size(0), -1)  #flattening the inputs. 
+        input = self.drop(input)
         input = F.relu(self.fc_layer_1(input))
+        input = self.drop(input)
         input = self.fc_layer_2(input)
         input = F.log_softmax(input, dim=1)  
         return input 
   
 
-    # def forward(self, input):
-    #     input = input[-2]
-    #     print(input.shape)
-    #     self.hid1 = torch.tanh(self.lay1(input))
-    #     layer2Input = torch.cat([input, self.hid1], dim = 1)
-    #     self.hid2 = torch.tanh(self.lay2(layer2Input))
-    #     outInput = torch.cat([self.hid2, self.hid1, input], dim = 1)
-    #     return torch.sigmoid(self.lay3(outInput))
-
 net = Network()
+# Need to convert to pytorch check this link for how to do it
+# https://discuss.pytorch.org/t/pytorch-equivalent-of-keras/29412
+# keras.models.Sequential([
+#     keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+#     keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+#     keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+#     keras.layers.Flatten(),
+#     keras.layers.Dense(4096, activation='relu'),
+#     keras.layers.Dropout(0.5),
+#     keras.layers.Dense(4096, activation='relu'),
+#     keras.layers.Dropout(0.5),
+#     keras.layers.Dense(10, activation='softmax')
+# ])
     
 ############################################################################
 ######      Specify the optimizer and loss function                   ######
 ############################################################################
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5)
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
 
 def loss_def(input, target):
-    return F.nll_loss(input, target, reduction='sum')
+    return F.cross_entropy(input, target)
 
 loss_func = loss_def
 
@@ -109,4 +158,4 @@ scheduler = None
 dataset = "./data/data"
 train_val_split = 0.8
 batch_size = 200
-epochs = 10
+epochs = 100
